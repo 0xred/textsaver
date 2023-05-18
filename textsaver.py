6aeb01d76b2c506846b6b91d4078ded8516876bb
0xredshadow <alblowy1@gmail.com>
2023-05-19 00:50:01 +0300

    Update textsaver.py

--- textsaver.py
+++ textsaver.py
@@ -1,11 +1,11 @@
 import os
-
+###################################################################################
 def savetotxt(savelineintxt,nametxtfile): # add line to text file
     with open(nametxtfile, "a") as afile:
         afile.write(savelineintxt + '\n')
         afile.close()
         return savelineintxt,nametxtfile
-    
+##################################################################################
 def removefromtxt(removeline,nametxtfile): # remove line from text file
     lines_to_keep = []
     with open(nametxtfile, "r") as rfile:
@@ -17,3 +17,14 @@ def removefromtxt(removeline,nametxtfile): # remove line from text file
         rfile.writelines(lines_to_keep)
         rfile.close()
         return removeline,nametxtfile
+###################################################################################
+def check_name_in_file(nametxtfile, findlineintxt): # find line if exist or not
+    with open(nametxtfile, "r") as f:
+        return findlineintxt in f.read()
+def check_name_exists(nametxtfile,findlineintxt,xfind):
+    exists = check_name_in_file(nametxtfile, findlineintxt)
+    if exists:
+        xfind = "found"
+    else:
+        xfind = "notfound"
+    return xfind
